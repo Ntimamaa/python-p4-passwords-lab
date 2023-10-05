54cb6b8aff3f6874377fa3a7bccdb207b2181a22
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-10-05 08:30:40 +0300

    Commited

--- server/app.py
+++ server/app.py
@@ -1,43 +1,88 @@
 #!/usr/bin/env python3
 
-from flask import request, session
-from flask_restful import Resource
+from flask import Flask, request, session
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from config import app, db, api
-from models import User
+app = Flask(__name__)
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class ClearSession(Resource):
-
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
-        db.session.add(user)
+        username = json['username']
+        password = json['password']
+
+        # Check if the username already exists
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return {'message': 'Username already exists'}, 400
+
+        # Create a new user and save it to the database
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
         db.session.commit()
-        return user.to_dict(), 201
+
+        # Save the user's ID in the session
+        session['user_id'] = new_user.id
+
+        return new_user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        username = json['username']
+        password = json['password']
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'message': 'Invalid credentials'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
